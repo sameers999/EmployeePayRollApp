87fa9633b83f16ed0050ecfd6db311fd0548c0e0
Syedsameer <syedmahammadsameer999@gmail.com>
2022-05-26 12:44:12 +0530

    Added: remaining properties to

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,22 +1,29 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.ToString;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.List;
 
-public class EmployeePayrollDTO {
+public @ToString class EmployeePayrollDTO {
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee Name Invalid")
     public String name;
 
     @Min(value = 500, message = "Min Wage should be more than 500")
     public long salary;
 
-    public EmployeePayrollDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
+    public String gender;
 
-    @Override
-    public String toString() {
-        return "EmployeePayrollDTO [name=" + name + ", salary=" + salary + "]";
-    }
+    @JsonFormat(pattern = "dd MMM yyyy")
+    public LocalDate startDate;
+
+    public String note;
+
+    public String profilePic;
+
+    public List<String> department;
+
 }
--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -3,18 +3,33 @@ package com.bridgelabz.employeepayrollapp.model;
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public @Data class EmployeePayrollData {
     private int employeeId;
     private String name;
     private long salary;
+    private String gender;
+    private LocalDate startDate;
+    private String note;
+    private String profilePic;
+    private List<String> department;
 
-    public  EmployeePayrollData() {
-
-    }
+    public EmployeePayrollData() {}
 
     public EmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
         this.employeeId = empId;
+        this.updateEmployeePayrollData(empPayrollDTO);
+    }
+
+    public void updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
         this.name = empPayrollDTO.name;
         this.salary = empPayrollDTO.salary;
+        this.gender = empPayrollDTO.gender;
+        this.note = empPayrollDTO.note;
+        this.startDate = empPayrollDTO.startDate;
+        this.profilePic = empPayrollDTO.profilePic;
+        this.department = empPayrollDTO.department;
     }
 }
