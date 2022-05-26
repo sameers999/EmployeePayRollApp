0ceb3fb4b2be38c3b56bb6785dfb18158c9ab009
Syedsameer <syedmahammadsameer999@gmail.com>
2022-05-26 09:27:22 +0530

    Added: DataBase Setting EnvironmentVariable

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollAppApplication.java
@@ -13,7 +13,8 @@ public class EmployeePayRollAppApplication {
         ApplicationContext context=SpringApplication.run(EmployeePayRollAppApplication.class, args);
         log.info("Employee Payroll App Started in {} Environment",
                 context.getEnvironment().getProperty("environment"));
-
+        log.info("Employee Payroll DB User is {} ",
+                context.getEnvironment().getProperty("spring.datasource.username"));
 
         System.out.println("Welcome to EmployeePayRollApp");
 
--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.bridgelabz.employeepayrollapp.service.IEmployeePayrollService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -36,7 +37,7 @@ public class EmployeePayrollController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO) {
+    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@Valid @RequestBody EmployeePayrollDTO empPayrollDTO) {
         EmployeePayrollData empData = null;
         empData = employeePayrollService.createEmployeePayrollData(empPayrollDTO);
         ResponseDTO respDTO = new ResponseDTO("Created Employee Payroll Data Successfully", empData);
@@ -45,7 +46,7 @@ public class EmployeePayrollController {
 
 
     @PutMapping("/update/{empId}")
-    public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId, @RequestBody EmployeePayrollDTO empPayrollDTO) {
+    public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId, @Valid @RequestBody EmployeePayrollDTO empPayrollDTO) {
         EmployeePayrollData empData = null;
         empData = employeePayrollService.updateEmployeePayrollData(empId, empPayrollDTO);
         ResponseDTO respDTO = new ResponseDTO("Updated Employee Payroll Data Successfully", empData);
